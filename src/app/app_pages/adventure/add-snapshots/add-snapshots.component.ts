2dba116c2d827069efbdd2fc44797308a8714158
Borayvor <Boray@abv.bg>
2017-01-03 11:10:48 +0200

    update

--- src/app/app_pages/adventure/add-snapshots/add-snapshots.component.ts
+++ src/app/app_pages/adventure/add-snapshots/add-snapshots.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 import { AdventureService } from './../../../app_core/services/adventure.service';
+import { AlertService } from './../../../app_core/services/alert.service';
+import { AuthenticationService } from './../../../app_core/services/authentication.service';
 
 import { AdventureDataCreateModel } from '../../../app_core/models/adventure-data-create.model';
 import { UserProfileModel } from './../../../app_core/models/user-profile.model';
@@ -10,23 +12,30 @@ import { UserProfileModel } from './../../../app_core/models/user-profile.model'
   templateUrl: './add-snapshots.component.html',
   styleUrls: ['./add-snapshots.component.css']
 })
-export class AddSnapshotsComponent implements OnInit {
+export class AddSnapshotsComponent implements OnInit, Input {
   private count: number;
   private model: AdventureDataCreateModel;
   private currentUser: UserProfileModel;
 
   constructor(
-    private adventureService: AdventureService
+    private adventureService: AdventureService,
+    private alertService: AlertService,
+    private authenticationService: AuthenticationService
   ) {
     this.count = 1
     this.model = new AdventureDataCreateModel;
   }
 
+  @Input() adventureId: string;
+
   ngOnInit() {
     
   }
 
-  addAdventureImage() {
+  addAdventurePhoto() {
+    this.currentUser = this.authenticationService.getCurrentUser();
+    this.model.ownerId = this.currentUser.objectId;
+    this.adventureService.addSnapshotToAdventure(this.adventureId, this.model);
   }
 
 }
